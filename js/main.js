44e045dd1da02c069e10fa0debbe876455fc2af9
mealok <70484146+mealok@users.noreply.github.com>
2021-08-18 09:21:02 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,12 @@ const responsive = {
         items: 3
     }
 }
-
+<script type="text/javascript" language="javascript">
+      var aax_size='300x600';
+      var aax_pubname = 'jiwanhacker02-21';
+      var aax_src='302';
+    </script>
+    <script type="text/javascript" language="javascript" src="http://c.amazon-adsystem.com/aax2/assoc.js"></script>
 $(document).ready(function () {
 
     $nav = $('.nav');
